cc4b6acfda3a7e266feccbfc22874adb2602f4a2
acho01 <archil.sharashenidze@azry.com>
2021-05-23 00:13:28 +0400

    daily page implemented

--- app/src/main/java/com/asharashenidze/weatherapp/MainActivity.kt
+++ app/src/main/java/com/asharashenidze/weatherapp/MainActivity.kt
@@ -3,14 +3,14 @@ package com.asharashenidze.weatherapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.viewpager2.widget.ViewPager2
 import com.asharashenidze.weatherapp.adapters.ViewPagerAdapter
 import com.asharashenidze.weatherapp.fragments.HourFragment
 import com.asharashenidze.weatherapp.fragments.TodayFragment
-import com.asharashenidze.weatherapp.service.WeatherService
 import com.asharashenidze.weatherapp.service.WeatherServiceClient
 
-class MainActivity : AppCompatActivity(), WeatherServiceClient.OnWeatherResponseListener  {
+class MainActivity : AppCompatActivity(), WeatherServiceClient.OnWeatherResponseListener {
 
     private lateinit var todayButton: ImageView
     private lateinit var hourButton: ImageView
@@ -38,6 +38,7 @@ class MainActivity : AppCompatActivity(), WeatherServiceClient.OnWeatherResponse
 
         initFragments()
         initButtons()
+        weatherService.getDailyWeather("Tbilisi", this)
 
     }
 
@@ -71,7 +72,23 @@ class MainActivity : AppCompatActivity(), WeatherServiceClient.OnWeatherResponse
     }
 
     override fun onDailyWeatherResponse(dailyWeatherResponse: DailyWeatherResponse) {
-        val s = 12
+        val temperature = findViewById<TextView>(R.id.text_temperature)
+        val description = findViewById<TextView>(R.id.text_weather_description)
+        val city = findViewById<TextView>(R.id.text_city)
+        val icon = findViewById<ImageView>(R.id.image_weather)
+        val details_temperature = findViewById<TextView>(R.id.text_temperature_value)
+        val details_feels = findViewById<TextView>(R.id.text_feelslike_value)
+        val details_humidity = findViewById<TextView>(R.id.text_humidity_value)
+        val details_pressure = findViewById<TextView>(R.id.text_pressure_value)
+
+        temperature.text = (Math.round(dailyWeatherResponse.main.temp).toString() + "\u00B0")
+        description.text = (dailyWeatherResponse.weather.get(0).description)
+        city.text = (dailyWeatherResponse.name)
+        IconProvider.setImageInto(dailyWeatherResponse.weather.get(0).icon, icon)
+        details_temperature.text = (Math.round(dailyWeatherResponse.main.temp).toString() + "\u00B0")
+        details_feels.text = (Math.round(dailyWeatherResponse.main.feels_like).toString() + "\u00B0")
+        details_humidity.text = (dailyWeatherResponse.main.humidity.toString() + "%")
+        details_pressure.text = (dailyWeatherResponse.main.pressure.toString())
     }
 
     private fun initNavigationButtons() {
